3088aae48ccc4b35ad3847e6f9aab3a76377e74f
Rui Lopes <rgl@ruilopes.com>
2021-07-31 09:52:33 +0100

    upgrade to qmp-shell 2.0.1

--- provision-qmp-shell.sh
+++ provision-qmp-shell.sh
@@ -3,8 +3,7 @@ source /vagrant/lib.sh
 
 # install.
 # see https://github.com/0xef53/qmp-shell
-# TODO lock the version.
-go get -v github.com/0xef53/qmp-shell
+go get -v github.com/0xef53/qmp-shell@582a5ac813191044ae2930ad278b179235ab6d7c # v2.0.1 2020-06-20T09:35:43Z
 
 # copy to the host.
 install -d /vagrant/tmp
