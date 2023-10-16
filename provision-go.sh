f1ba4efb1f151089aa9212ec96eccc15b129e7fe
Rui Lopes <rgl@ruilopes.com>
2023-10-16 06:28:06 +0100

    upgrade to go 1.21.3

--- provision-go.sh
+++ provision-go.sh
@@ -2,10 +2,10 @@
 source /vagrant/lib.sh
 
 # install go.
-# see https://golang.org/dl/
-# see https://golang.org/doc/install
-artifact_url=https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
-artifact_sha=dab7d9c34361dc21ec237d584590d72500652e7c909bf082758fb63064fca0ef
+# see https://go.dev/dl/
+# see https://go.dev/doc/install
+artifact_url=https://go.dev/dl/go1.21.3.linux-amd64.tar.gz
+artifact_sha=1241381b2843fae5a9707eec1f8fb2ef94d827990582c7c7c32f5bdfbfd420c8
 artifact_path="/tmp/$(basename $artifact_url)"
 wget -qO $artifact_path $artifact_url
 if [ "$(sha256sum $artifact_path | awk '{print $1}')" != "$artifact_sha" ]; then
