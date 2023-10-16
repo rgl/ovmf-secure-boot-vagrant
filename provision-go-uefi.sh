100ed67f1668f56666c67d07893f3734b09e443b
Rui Lopes <rgl@ruilopes.com>
2023-10-16 06:29:17 +0100

    upgrade to go-uefi efianalyze 2023-08-08T20:18:20Z

--- provision-go-uefi.sh
+++ provision-go-uefi.sh
@@ -3,4 +3,4 @@ source /vagrant/lib.sh
 
 # install.
 # see https://github.com/Foxboron/go-uefi
-go install -v github.com/foxboron/go-uefi/cmd/efianalyze@6b0ee85df4cd4bfe24a2bdf2120f0f9ffe029841 # 2021-09-29T17:09:05Z
+go install -v github.com/foxboron/go-uefi/cmd/efianalyze@18b9ba9cd4c3e6ff3f8ac11a3102b1ec8b85fe51 # 2023-08-08T20:18:20Z
